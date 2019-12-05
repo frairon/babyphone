8a73120e4162d0c9b594fdaa0de668b64616bd23
frairon <franz.eichhorn@lovoo.com>
2019-12-05 06:47:33 +0100

    server: add some unit tests

--- server/server.go
+++ server/server.go
@@ -1,55 +1,89 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"golang.org/x/sync/errgroup"
 )
+
+type Config struct {
+	LoginDelay time.Duration
+}
 
 type Server struct {
 	upgrader websocket.Upgrader
 	m        sync.Mutex
 
 	spaces map[string]*Space
+	cfg    *Config
 }
 
 type Space struct {
 	writeM      sync.Mutex
+	upgrader    websocket.Upgrader
+	server      *Server
 	password    string
-	connections map[string]*Connection
+	name        string
+	connections map[*Connection]bool
+	cfg         *Config
+}
+
+func NewConfig() *Config {
+	return &Config{
+		LoginDelay: 2 * time.Second,
+	}
 }
 
 func (s *Space) shutdown(msg string) error {
 	s.writeM.Lock()
 	defer s.writeM.Unlock()
 	var errs errgroup.Group
-	for name, conn := range s.connections {
+	for conn := range s.connections {
 		errs.Go(func() error {
-			log.Printf("shutting down connection %s", name)
+			log.Printf("shutting down connection %v", conn.String())
 			return conn.shutdown(msg)
 		})
 	}
 	return errs.Wait()
 }
 
-func New() *Server {
+func New(cfg *Config) *Server {
 	return &Server{
+		cfg:    cfg,
 		spaces: make(map[string]*Space),
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
 	}
 }
 
 func (s *Server) AddSpace(name, password string) {
+	if _, exists := s.spaces[name]; exists {
+		panic(fmt.Sprintf("duplicate space name %s", name))
+	}
 	s.spaces[name] = &Space{
-		password:    password,
-		connections: make(map[string]*Connection),
+		cfg:         s.cfg,
+		name:        name,
+		upgrader:    s.upgrader,
+		password:    hashPassword(password),
+		server:      s,
+		connections: make(map[*Connection]bool),
 	}
 }
 
 func (s *Server) Start(addr string) (chan struct{}, *http.Server) {
-	http.HandleFunc("/", s.OnConnect)
+	for name, space := range s.spaces {
+		http.HandleFunc(fmt.Sprintf("/%s", name), space.OnConnect)
+	}
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
 	srv := &http.Server{Addr: addr, Handler: http.DefaultServeMux}
 	srv.RegisterOnShutdown(s.Shutdown)
 	done := make(chan struct{})
@@ -57,7 +91,7 @@ func (s *Server) Start(addr string) (chan struct{}, *http.Server) {
 		defer close(done)
 		log.Printf("Starting server at %s", addr)
 		err := srv.ListenAndServe()
-		if err != nil {
+		if err != nil && err != http.ErrServerClosed {
 			log.Printf("Error running server: %v", err)
 		}
 	}()
@@ -65,25 +99,34 @@ func (s *Server) Start(addr string) (chan struct{}, *http.Server) {
 	return done, srv
 }
 
-func (s *Server) OnConnect(w http.ResponseWriter, r *http.Request) {
+func (s *Space) OnConnect(w http.ResponseWriter, r *http.Request) {
 	c, err := s.upgrader.Upgrade(w, r, nil)
+	log.Printf("Space %s got connection, upgrading to websocket", s.name)
 	if err != nil {
-		log.Print("Upgrading to websockets failed", err)
+		log.Printf("Upgrading to websockets failed: %v", err)
 		return
 	}
-	s.m.Lock()
-	defer s.m.Unlock()
+	s.writeM.Lock()
+	defer s.writeM.Unlock()
 	conn := &Connection{
-		c: c,
+		cfg:      s.cfg,
+		creation: time.Now(),
+		space:    s,
+		c:        c,
+		state:    unboundConnState,
 	}
 	log.Printf("creating Connection")
-	// if _, exists := s.connections[conn]; exists {
-	// 	panic("Connection already present. This must not happen.")
-	// }
-	// s.connections[conn] = true
+	s.connections[conn] = true
 	go func() {
-		defer conn.Close()
-		conn.handleMessages()
+		defer func() {
+			conn.Close()
+			s.writeM.Lock()
+			defer s.writeM.Unlock()
+			delete(s.connections, conn)
+		}()
+		log.Printf("running connection")
+		conn.run()
+		log.Printf("connection done")
 	}()
 }
 
--- server/server_test.go
+++ server/server_test.go
@@ -1,37 +1,150 @@
 package server
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 const (
-	address = "frosi-babyphone.appspot.com:8080"
+	address = "localhost:8080"
 )
+
+var (
+	disconnected = &Message{
+		Action: "__DISCONNECTED__",
+	}
+)
+
+type testConnection struct {
+	c *websocket.Conn
+}
+
+func (tc *testConnection) Close() {
+	err := tc.c.WriteControl(websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseGoingAway, "test is over"),
+		time.Now().Add(100*time.Millisecond))
+	if err != nil {
+		log.Printf("Error closing test connection: %v", err)
+	}
+	err = tc.c.Close()
+	if err != nil {
+		log.Printf("Error closing underlying connection: %v", err)
+	}
+}
+
+func (tc *testConnection) writeMessage(t *testing.T, msg *Message) {
+	marshalled, err := json.Marshal(msg)
+	assert.Nil(t, err)
+	err = tc.c.WriteMessage(websocket.TextMessage, marshalled)
+	assert.Nil(t, err)
+}
+
+func (tc *testConnection) nextMessage(t *testing.T) *Message {
+	done := make(chan struct{})
+	var msg *Message
+	go func() {
+		defer close(done)
+		var readMsg Message
+		_, rawMsg, err := tc.c.ReadMessage()
+		if err != nil {
+			if _, ok := err.(*websocket.CloseError); ok {
+				msg = disconnected
+			} else {
+				log.Printf("Error reading message: %v", err)
+			}
+			return
+		}
+		err = json.Unmarshal(rawMsg, &readMsg)
+		if err != nil {
+			log.Printf("Error unmarshalling message: %v", err)
+		}
+		msg = &readMsg
+	}()
+	select {
+	case <-done:
+		return msg
+	case <-time.After(2 * time.Second):
+		t.Fatalf("Message read timed out")
+	}
+	return nil
+}
+func waitForServer() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		resp, err := http.Get(fmt.Sprintf("http://%s/test", address))
+		if err == nil && resp.StatusCode == 200 {
+			break
+		}
+	}
+}
 
 func TestFirst(t *testing.T) {
 	log.SetFlags(0)
 
-	// server := New()
-	// done, srv := server.Start(address)
+	cfg := NewConfig()
+	cfg.LoginDelay = 0
 
-	// defer func() {
-	// 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// 	defer cancel()
-	// 	srv.Shutdown(ctx)
-	// 	<-done
-	// }()
+	server := New(cfg)
+	server.AddSpace("test-space", "test123")
+	done, srv := server.Start(address)
+	waitForServer()
 
-	u := url.URL{Scheme: "ws", Host: address, Path: "/"}
-	log.Printf("connecting to %s", u.String())
+	defer func() {
+		// give the unit tests some time to finish before killing the server
+		time.Sleep(100 * time.Millisecond)
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			log.Printf("Error shutting down server: %v", err)
+		}
+		<-done
+	}()
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	require.Nil(t, err)
-	defer c.Close()
-	c.WriteMessage(websocket.TextMessage, []byte("hello world"))
-	c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	u := url.URL{Scheme: "ws", Host: address, Path: "test-space"}
+
+	dialer := &websocket.Dialer{
+		Proxy:            http.ProxyFromEnvironment,
+		HandshakeTimeout: 1 * time.Second,
+	}
+
+	t.Run("invalid-url", func(t *testing.T) {
+		_, _, err := dialer.Dial(fmt.Sprintf("ws://%s/inexistent-space", address), nil)
+		require.NotNil(t, err)
+	})
+
+	t.Run("setup_passwd_fail", func(t *testing.T) {
+		c, _, err := dialer.Dial(u.String(), nil)
+		conn := &testConnection{
+			c: c,
+		}
+		require.Nil(t, err)
+		time.Sleep(200 * time.Millisecond)
+		defer conn.Close()
+
+		conn.writeMessage(t, &Message{
+			Action: "setup",
+			Setup: &Setup{
+				Name:     "test",
+				Password: "test123",
+				Type:     clientConnection,
+			},
+		})
+		time.Sleep(100 * time.Millisecond)
+		msg := conn.nextMessage(t)
+		log.Printf("msg %#v", msg)
+		assert.True(t, msg != nil)
+		assert.Equal(t, msg.Action, "ok")
+	})
+	// c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 }
